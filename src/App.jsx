17625f3d158a854e19c2203a15d8a44415d2ab5d
Mehmet <mathdenizi@gmail.com>
2022-10-30 21:24:48 +0100

    Learning Router 6.4

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import BlogLayout from "./pages/BlogLayout";
+import BlogPostsPage, { loader as blogPostsLoader } from "./pages/BlogPosts";
+import NewPostPage from "./pages/NewPost";
+import PostDetailPage, { loader as blogPostLoader } from "./pages/PostDetail";
+import RootLayout from "./components/RootLayout";
+import WelcomePage from "./pages/Welcome";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+      <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route
+          path=":id"
+          element={<PostDetailPage />}
+          loader={blogPostLoader}
+        />
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} />
+    </Route>
+  )
+);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import MainNavigation from "../components/MainNavigation";
+
+function ErrorPage() {
+  return (
+    <>
+      <MainNavigation />
+      <main id="error-content">
+        <h1> An error occured</h1>
+      </main>
+    </>
+  );
+}
+
+export default ErrorPage;
